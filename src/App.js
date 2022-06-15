26e4e44e6747a4da587d0a535f1eb0f2980398ba
SilverFlin <toledorusso@outlook.com>
2022-06-14 17:55:33 -0700

    js to jsx extension

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import React, { useState } from 'react';
 import Game from './Game/Game';
 import Homepage from './About/Homepage';
 
+import { Link } from 'react-router-dom';
+
 import NavBar from './UI/NavBar';
 import Footer from './UI/Footer';
 
 function App() {
 	return (
 		<div>
+			<div>
+				<h1>Bookkeeper</h1>
+				<nav
+					style={{
+						borderBottom: 'solid 1px',
+						paddingBottom: '1rem'
+					}}
+				>
+					<Link to="/invoices">Invoices</Link> | <Link to="/expenses">Expenses</Link>
+				</nav>
+			</div>
 			<NavBar />
 			<Game />
 			<Homepage />
